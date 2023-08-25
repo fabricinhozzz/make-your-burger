61b879c4f2368f87862db25ea8111d221a42052f
fabricinho <fabricio.coding@gmail.com>
2023-08-25 16:21:46 -0300

    create

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <footer id="footer">
+        <p>Make Your Burguer &copy; 2023</p>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+  #footer {
+    height: 80px;
+    background-color: #222;
+    border-top: 2px solid #111;
+    color: #fcba03;
+    display: flex;
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    width: 100%;
+  }
+</style>
